e28413f37836ab19861fd5de1de3f58c74b990b9
Dario Passarello <dario.passarello@gmail.com>
2023-05-20 18:30:27 +0200

    fix: cardinality 1 misplaced

--- src/classes.py
+++ src/classes.py
@@ -13,6 +13,17 @@ from utils import sort_dict
 
 
 def generate_classes_models_old(results : Iterable[AnalyzedTensor], args : Args):
+    """
+    Calculates and create the three files containing the CLASSES framework model
+
+    results
+    ---
+    List of all the analyzed experimental results (AnalyzedTensor)
+
+    args
+    ---
+    The Args object containing all the program options, used for getting the output folder
+    """
     cardinalities, spatial_models = calculate_classes_cardinalities_and_spatial(results)
     value_analysis = calculate_value_analysis(results)
     save_classes_models(cardinalities, spatial_models, value_analysis, args)
@@ -47,36 +58,47 @@ def save_classes_models(cardinality_model_json : dict, spatial_model_json : dict
 
 def calculate_classes_cardinalities_and_spatial(results : Iterable[AnalyzedTensor]) -> Tuple[Dict[int, list], Dict[int, Dict[str, Dict[str, float]]]]:
     total_count = len(results)
+    # Dictionary containing all the conts 
     cardinalities = count_by(results, key=lambda x: x.corrupted_values_count)    
     classes_cardinalities_json = {cardinality : [count, count / total_count] for cardinality, count in cardinalities.items()} 
 
     results_by_cardinality = group_by(results, key=lambda x: x.corrupted_values_count)
-
+    # Maximum threshold for relative frequencies inside the patterns
+    # All the patterns that have a relative frequency lower than that will be considered random
     RANDOM_THRESHOLD = 0.05
 
     classes_spatial_json : Dict[int, Dict[str, Dict[str, float]]] = OrderedDict()
 
     for cardinality, cardinality_results in results_by_cardinality.items():
         if cardinality == 1:
-            classes_cardinalities_json[1] = {"RANDOM": 1.0}
+            classes_spatial_json[1] = {"RANDOM": 1.0}
             continue
+
         cardinality_total_count = cardinalities[cardinality]
         results_by_sp_class = group_by(cardinality_results, key=lambda x: x.spatial_class)
-
+        # Key: Spatial Class ID, Value: Relative Frequqncy of the spatial class given the current cardinality
         ff_spatial_json = OrderedDict()
+        # Key: Spatial Class ID, Value: A dictionary
+        # The sub-dictionary contains
+        # Keys: The spatial pattern (parameters of the pattern) or "RANDOM" or "MAX"
+        # Values: Relative frequency of the spatial pattern given the current cardinality and the spatial pattern
         pf_spatial_json = OrderedDict()
         for sp_class, sp_class_results in results_by_sp_class.items():
+            # Get the key of the FF/PF dicts: The id of the spatial pattern used in CLASSES
             sp_class_classes_id = to_classes_id(sp_class.display_name())
+            # Set the relative frequency of the pattern
             ff_spatial_json[sp_class_classes_id] = len(sp_class_results) / cardinality_total_count
-
+            # Number of tensors that have the current cardinality and current spatial class
             sp_pattern_total_count = len(sp_class_results)
+            # Count the number of occurence of each pattern
             sp_pattern_counts = count_by(sp_class_results, key=lambda x: x.spatial_pattern)
             sp_pattern_counts_classes = {str(pattern) : count / sp_pattern_total_count for pattern, count in sp_pattern_counts.items() if count / sp_pattern_total_count >= RANDOM_THRESHOLD}
-            
+            # Count the number of patterns that fall into the wildcard category "RANDOM" (relative frequency < RANDOM_THRESHOLD)
             random_counts = sum(count for count in sp_pattern_counts.values() if count / sp_pattern_total_count < RANDOM_THRESHOLD)
 
             sp_class_results_max = [x.spatial_class_params.get("MAX", []) for x in sp_class_results]
             sp_class_results_max = [x if isinstance(x, list) else [x] for x in sp_class_results_max]
+            # Calculate the "MAX" vector, useful for generating "RANDOM" patterns inside CLASSES
             max_vector = reduce(accumulate_max, sp_class_results_max)
             sp_pattern_counts_classes = sp_pattern_counts_classes | {"MAX": max_vector, "RANDOM": random_counts / sp_pattern_total_count}
 
